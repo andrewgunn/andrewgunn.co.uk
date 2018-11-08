2e06d0b0d14346019de1a37a9c26097e3bf6305a
Andrew Gunn <hello@andrewgunn.co.uk>
2018-11-08 15:08:53 +0000

    Don't open external links in a new tab

--- assets/main.js
+++ assets/main.js
@@ -5,14 +5,3 @@
 (function (hljs) {
     hljs.initHighlightingOnLoad();
 })(window.hljs);
-
-(function () {
-    $(document).on('click', 'a', function (event) {
-        if (!new RegExp('/^https://' + window.location.host + '/').test(this.href)) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        window.open(this.href, '_blank');
-        }
-    });
-})(window.jQuery);
