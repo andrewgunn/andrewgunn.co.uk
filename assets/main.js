ef06c70f906166aa77d565f0d5025e2b42558829
Andrew Gunn <hello@andrewgunn.co.uk>
2018-11-08 21:59:46 +0000

    Add GA

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,14 @@
 # Front matter comment to ensure Jekyll properly reads file.
 ---
 
+;
 (function (hljs) {
     hljs.initHighlightingOnLoad();
 })(window.hljs);
+
+(function (console) {
+    if (console && console.log) {
+        console.log("%c👋 Hello! Welcome to Andrew Gunn's Blog. The code can be found on GitHub.", "background-color: #303030; color: #f15a5d; font-weight: bold; font-size: 16px; padding: 1rem;")
+        console.log("%chttps://github.com/andrewgunn/andrewgunn.co.uk", "color: #f15a5d; font-weight: bold; font-size: 16px;")
+    }
+})(window.console);
